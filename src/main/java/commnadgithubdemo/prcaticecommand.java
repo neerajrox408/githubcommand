ae8655657b8840a32ad90b98150742b8ac4a5a50
neerajrox408 <109574809+neerajrox408@users.noreply.github.com>
2022-07-25 12:45:47 +0530

    Update prcaticecommand.java

--- src/main/java/commnadgithubdemo/prcaticecommand.java
+++ src/main/java/commnadgithubdemo/prcaticecommand.java
@@ -3,7 +3,17 @@ package commnadgithubdemo;
 public class prcaticecommand {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
+		system.out.println("hello");
+		String browser="chrome";
+if(browser=="chrome")
+{
+	System.out.println("browser is valid");
+}
+else
+{
+	System.out.println("browser not valid");
+}
+
 
 	}
 
